c1ff024e7ae50726115ae54aab49053214503e28
Nikita Zhukov <zhukov@vigroup.ru>
2017-09-06 22:22:23 +0500

    finish 2 add task with flipped images

--- cv_school_test/project/Task.py
+++ cv_school_test/project/Task.py
@@ -6,6 +6,7 @@ imagesPath = os.path.join("..", "images")
 annoFilePath = os.path.join("..", "annotations")
 fragmentsFilePath = os.path.join("..", "fragments")
 fragmentsGrayFilePath = os.path.join("..", "fragments_greyscale")
+fragmentsFlipFilePath = os.path.join("..", "fragments_flip")
 
 partFileNameList = ["person_292", "person_and_bike_125", "person_and_bike_132"]
 
@@ -33,6 +34,15 @@ def task1_grayscale():
             image.save(os.path.join(fragmentsGrayFilePath, name + "_gray" + os.extsep + "png"))
 
 
+def task2_flip():
+    for fileName in os.listdir(fragmentsFilePath):
+        name, ext = os.path.splitext(fileName)
+        if ext == ".png":
+            image = Image.open(os.path.join(fragmentsFilePath, fileName)).transpose(Image.FLIP_LEFT_RIGHT)
+            image.save(os.path.join(fragmentsFlipFilePath, name + "_flip" + os.extsep + "png"))
+
+
 task0_crop()
 task1_grayscale()
+task2_flip()
 
